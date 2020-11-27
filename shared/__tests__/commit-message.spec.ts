18c54e54affea527d55c5ce32a0a85dce7a353a1
Jeffrey Ko <jeffreyko@gmail.com>
2020-11-27 23:40:50 +0700

    feat: :sparkles: support unicode emojis in subject

--- shared/__tests__/commit-message.spec.ts
+++ shared/__tests__/commit-message.spec.ts
@@ -1,228 +1,331 @@
 import { QualifiedConfig } from '@commitlint/types';
-import { GitmojiDefinition } from 'shared/presets/gitmojis';
-import { TypeDefinition } from 'shared/presets/types';
 
 import { Commit } from '../../dtos/lint-result.dto';
 import { CommitMessageLib } from '../commit-message.lib';
+import { GitmojiDefinition, GITMOJIS } from '../presets/gitmojis';
+import { TypeDefinition } from '../presets/types';
 
 describe('commit-message', () => {
-  test('format() - body-max-line-length - single line', () => {
-    const commit: Commit = {
-      raw: 'chore: validate commit message\r\n\r\nLorem ipsum dolor sit amet',
-      header: 'chore: validate commit message',
-      type: 'chore:',
-      scope: null,
-      subject: null,
-      body: 'Lorem ipsum dolor sit amet',
-      footer: null,
-      mentions: [],
-      notes: [],
-      references: [],
-      revert: null,
-      merge: null,
-    };
+  /****************************************************
+   * format()
+   ****************************************************/
+  describe('format()', () => {
+    test('body-max-line-length - single line', () => {
+      const commit: Commit = {
+        raw: 'chore: validate commit message\r\n\r\nLorem ipsum dolor sit amet',
+        header: 'chore: validate commit message',
+        type: 'chore:',
+        scope: null,
+        subject: null,
+        body: 'Lorem ipsum dolor sit amet',
+        footer: null,
+        mentions: [],
+        notes: [],
+        references: [],
+        revert: null,
+        merge: null,
+      };
 
-    const config: QualifiedConfig = {
-      extends: [],
-      formatter: '',
-      rules: {
-        'body-max-line-length': [0, 'always', 25],
-      },
-      parserPreset: undefined,
-      ignores: [],
-      defaultIgnores: true,
-      plugins: undefined,
-    };
+      const config: QualifiedConfig = {
+        extends: [],
+        formatter: '',
+        rules: {
+          'body-max-line-length': [0, 'always', 25],
+        },
+        parserPreset: undefined,
+        ignores: [],
+        defaultIgnores: true,
+        plugins: undefined,
+      };
 
-    const result = CommitMessageLib.format(commit, config);
-    expect(result).toBe('chore: validate commit message\r\n\r\nLorem ipsum dolor sit\r\namet');
+      const result = CommitMessageLib.format(commit, config);
+      expect(result).toBe('chore: validate commit message\r\n\r\nLorem ipsum dolor sit\r\namet');
+    });
+
+    test('footer-max-line-length - single line', () => {
+      const commit: Commit = {
+        raw: 'chore: validate\r\n\r\nbody\r\n\r\nBREAKING CHANGE: Lorem ipsum dolor',
+        header: 'chore: validate',
+        type: 'chore:',
+        scope: null,
+        subject: null,
+        body: 'Lorem ipsum dolor sit amet',
+        footer: 'BREAKING CHANGE: Lorem ipsum dolor',
+        mentions: [],
+        notes: [],
+        references: [],
+        revert: null,
+        merge: null,
+      };
+
+      const config: QualifiedConfig = {
+        extends: [],
+        formatter: '',
+        rules: {
+          'footer-max-line-length': [0, 'always', 30],
+        },
+        parserPreset: undefined,
+        ignores: [],
+        defaultIgnores: true,
+        plugins: undefined,
+      };
+
+      const result = CommitMessageLib.format(commit, config);
+      expect(result).toBe(
+        'chore: validate\r\n\r\nbody\r\n\r\nBREAKING CHANGE: Lorem ipsum\r\ndolor',
+      );
+    });
   });
 
-  test('format() - footer-max-line-length - single line', () => {
-    const commit: Commit = {
-      raw: 'chore: validate\r\n\r\nbody\r\n\r\nBREAKING CHANGE: Lorem ipsum dolor',
-      header: 'chore: validate',
-      type: 'chore:',
-      scope: null,
-      subject: null,
-      body: 'Lorem ipsum dolor sit amet',
-      footer: 'BREAKING CHANGE: Lorem ipsum dolor',
-      mentions: [],
-      notes: [],
-      references: [],
-      revert: null,
-      merge: null,
-    };
+  /****************************************************
+   * setGitmoji()
+   ****************************************************/
+  describe('setGitmoji()', () => {
+    test('empty message', () => {
+      const message = '';
+      const gitmoji: GitmojiDefinition = {
+        icon: '🎉',
+        shortcode: ':tada:',
+        description: 'Begin a project.',
+      };
 
-    const config: QualifiedConfig = {
-      extends: [],
-      formatter: '',
-      rules: {
-        'footer-max-line-length': [0, 'always', 30],
-      },
-      parserPreset: undefined,
-      ignores: [],
-      defaultIgnores: true,
-      plugins: undefined,
-    };
+      const result = CommitMessageLib.setGitmoji(message, gitmoji);
+      expect(result).toBe(` ${gitmoji.shortcode} `);
+    });
 
-    const result = CommitMessageLib.format(commit, config);
-    expect(result).toBe('chore: validate\r\n\r\nbody\r\n\r\nBREAKING CHANGE: Lorem ipsum\r\ndolor');
+    test('empty message (unicode)', () => {
+      const message = '';
+      const gitmoji: GitmojiDefinition = {
+        icon: '🎉',
+        shortcode: ':tada:',
+        description: 'Begin a project.',
+      };
+
+      const result = CommitMessageLib.setGitmoji(message, gitmoji, false);
+      expect(result).toBe(` ${gitmoji.icon} `);
+    });
+
+    test('type only', () => {
+      const message = 'chore:';
+      const gitmoji: GitmojiDefinition = {
+        icon: '🎉',
+        shortcode: ':tada:',
+        description: 'Begin a project.',
+      };
+
+      const result = CommitMessageLib.setGitmoji(message, gitmoji);
+      expect(result).toBe(`chore: ${gitmoji.shortcode} `);
+    });
+
+    test('type only (unicode)', () => {
+      const message = 'chore:';
+      const gitmoji: GitmojiDefinition = {
+        icon: '🎉',
+        shortcode: ':tada:',
+        description: 'Begin a project.',
+      };
+
+      const result = CommitMessageLib.setGitmoji(message, gitmoji, false);
+      expect(result).toBe(`chore: ${gitmoji.icon} `);
+    });
+
+    test('type and existing gitmoji', () => {
+      const message = 'chore: :rocket:';
+      const gitmoji: GitmojiDefinition = {
+        icon: '🎉',
+        shortcode: ':tada:',
+        description: 'Begin a project.',
+      };
+
+      const result = CommitMessageLib.setGitmoji(message, gitmoji);
+      expect(result).toBe(`chore: ${gitmoji.shortcode} `);
+    });
+
+    test('type and existing gitmoji (unicode)', () => {
+      const message = 'chore: :rocket:';
+      const gitmoji: GitmojiDefinition = {
+        icon: '🎉',
+        shortcode: ':tada:',
+        description: 'Begin a project.',
+      };
+
+      const result = CommitMessageLib.setGitmoji(message, gitmoji, false);
+      expect(result).toBe(`chore: ${gitmoji.icon} `);
+    });
+
+    test('type and subject', () => {
+      const message = 'chore: validate gitmoji';
+      const gitmoji: GitmojiDefinition = {
+        icon: '🎉',
+        shortcode: ':tada:',
+        description: 'Begin a project.',
+      };
+
+      const result = CommitMessageLib.setGitmoji(message, gitmoji);
+      expect(result).toBe(`chore: ${gitmoji.shortcode} validate gitmoji`);
+    });
+
+    test('type and subject (unicode)', () => {
+      const message = 'chore: validate gitmoji';
+      const gitmoji: GitmojiDefinition = {
+        icon: '🎉',
+        shortcode: ':tada:',
+        description: 'Begin a project.',
+      };
+
+      const result = CommitMessageLib.setGitmoji(message, gitmoji, false);
+      expect(result).toBe(`chore: ${gitmoji.icon} validate gitmoji`);
+    });
+
+    test('type, subject and existing gitmoji', () => {
+      const message = 'chore: :rocket: validate gitmoji';
+      const gitmoji: GitmojiDefinition = {
+        icon: '🎉',
+        shortcode: ':tada:',
+        description: 'Begin a project.',
+      };
+
+      const result = CommitMessageLib.setGitmoji(message, gitmoji);
+      expect(result).toBe(`chore: ${gitmoji.shortcode} validate gitmoji`);
+    });
+
+    test('type, subject and existing gitmoji (unicode)', () => {
+      const message = 'chore: :rocket: validate gitmoji';
+      const gitmoji: GitmojiDefinition = {
+        icon: '🎉',
+        shortcode: ':tada:',
+        description: 'Begin a project.',
+      };
+
+      const result = CommitMessageLib.setGitmoji(message, gitmoji, false);
+      expect(result).toBe(`chore: ${gitmoji.icon} validate gitmoji`);
+    });
+
+    test('type, subject and body', () => {
+      const message = 'chore: validate gitmoji\r\n\r\nthis is a body';
+      const gitmoji: GitmojiDefinition = {
+        icon: '🎉',
+        shortcode: ':tada:',
+        description: 'Begin a project.',
+      };
+
+      const result = CommitMessageLib.setGitmoji(message, gitmoji);
+      expect(result).toBe(`chore: ${gitmoji.shortcode} validate gitmoji\r\n\r\nthis is a body`);
+    });
+
+    test('type, subject and body (unicode)', () => {
+      const message = 'chore: validate gitmoji\r\n\r\nthis is a body';
+      const gitmoji: GitmojiDefinition = {
+        icon: '🎉',
+        shortcode: ':tada:',
+        description: 'Begin a project.',
+      };
+
+      const result = CommitMessageLib.setGitmoji(message, gitmoji, false);
+      expect(result).toBe(`chore: ${gitmoji.icon} validate gitmoji\r\n\r\nthis is a body`);
+    });
+
+    test('all unicode', () => {
+      const message = '';
+      const gitmoji: GitmojiDefinition = {
+        icon: '🎉',
+        shortcode: ':tada:',
+        description: 'Begin a project.',
+      };
+
+      GITMOJIS.forEach((x) => {
+        const result = CommitMessageLib.setGitmoji(`chore: ${x.icon} test`, gitmoji, false);
+        expect(`${result} - ${x.icon}`).toBe(`chore: ${gitmoji.icon} test - ${x.icon}`);
+      });
+    });
   });
 
-  test('setGitmoji() - empty message', () => {
-    const message = '';
-    const gitmoji: GitmojiDefinition = {
-      icon: '🎉',
-      shortcode: ':tada:',
-      description: 'Begin a project.',
-    };
+  /****************************************************
+   * setType()
+   ****************************************************/
+  describe('setType()', () => {
+    test('empty message', () => {
+      const message = '';
+      const type: TypeDefinition = {
+        key: 'fix',
+        title: 'Bug Fixes',
+        description: 'A bug fix',
+      };
 
-    const result = CommitMessageLib.setGitmoji(message, gitmoji);
-    expect(result).toBe(` ${gitmoji.shortcode} `);
-  });
+      const result = CommitMessageLib.setType(message, type);
+      expect(result).toBe(`${type.key}: `);
+    });
 
-  test('setGitmoji() - type only', () => {
-    const message = 'chore:';
-    const gitmoji: GitmojiDefinition = {
-      icon: '🎉',
-      shortcode: ':tada:',
-      description: 'Begin a project.',
-    };
+    test('existing type', () => {
+      const message = 'chore:';
+      const type: TypeDefinition = {
+        key: 'fix',
+        title: 'Bug Fixes',
+        description: 'A bug fix',
+      };
 
-    const result = CommitMessageLib.setGitmoji(message, gitmoji);
-    expect(result).toBe(`chore: ${gitmoji.shortcode} `);
-  });
+      const result = CommitMessageLib.setType(message, type);
+      expect(result).toBe(`${type.key}: `);
+    });
 
-  test('setGitmoji() - type and existing gitmoji', () => {
-    const message = 'chore: :rocket:';
-    const gitmoji: GitmojiDefinition = {
-      icon: '🎉',
-      shortcode: ':tada:',
-      description: 'Begin a project.',
-    };
+    test('gitmoji', () => {
+      const message = ':tada:';
+      const type: TypeDefinition = {
+        key: 'fix',
+        title: 'Bug Fixes',
+        description: 'A bug fix',
+      };
 
-    const result = CommitMessageLib.setGitmoji(message, gitmoji);
-    expect(result).toBe(`chore: ${gitmoji.shortcode} `);
-  });
+      const result = CommitMessageLib.setType(message, type);
+      expect(result).toBe(`${type.key}: :tada:`);
+    });
 
-  test('setGitmoji() - type and subject', () => {
-    const message = 'chore: validate gitmoji';
-    const gitmoji: GitmojiDefinition = {
-      icon: '🎉',
-      shortcode: ':tada:',
-      description: 'Begin a project.',
-    };
+    test('existing type and gitmoji', () => {
+      const message = 'chore: :tada:';
+      const type: TypeDefinition = {
+        key: 'fix',
+        title: 'Bug Fixes',
+        description: 'A bug fix',
+      };
 
-    const result = CommitMessageLib.setGitmoji(message, gitmoji);
-    expect(result).toBe(`chore: ${gitmoji.shortcode} validate gitmoji`);
-  });
+      const result = CommitMessageLib.setType(message, type);
+      expect(result).toBe(`${type.key}: :tada:`);
+    });
 
-  test('setGitmoji() - type, subject and existing gitmoji', () => {
-    const message = 'chore: :rocket: validate gitmoji';
-    const gitmoji: GitmojiDefinition = {
-      icon: '🎉',
-      shortcode: ':tada:',
-      description: 'Begin a project.',
-    };
+    test('existing type, gitmoji and subject', () => {
+      const message = 'chore: :tada: test message';
+      const type: TypeDefinition = {
+        key: 'fix',
+        title: 'Bug Fixes',
+        description: 'A bug fix',
+      };
 
-    const result = CommitMessageLib.setGitmoji(message, gitmoji);
-    expect(result).toBe(`chore: ${gitmoji.shortcode} validate gitmoji`);
-  });
+      const result = CommitMessageLib.setType(message, type);
+      expect(result).toBe(`${type.key}: :tada: test message`);
+    });
 
-  test('setGitmoji() - type, subject and body', () => {
-    const message = 'chore: validate gitmoji\r\n\r\nthis is a body';
-    const gitmoji: GitmojiDefinition = {
-      icon: '🎉',
-      shortcode: ':tada:',
-      description: 'Begin a project.',
-    };
+    test('existing type, gitmoji, subject and body', () => {
+      const message = 'chore: :tada: test message\r\n\r\nbody';
+      const type: TypeDefinition = {
+        key: 'fix',
+        title: 'Bug Fixes',
+        description: 'A bug fix',
+      };
 
-    const result = CommitMessageLib.setGitmoji(message, gitmoji);
-    expect(result).toBe(`chore: ${gitmoji.shortcode} validate gitmoji\r\n\r\nthis is a body`);
-  });
+      const result = CommitMessageLib.setType(message, type);
+      expect(result).toBe(`${type.key}: :tada: test message\r\n\r\nbody`);
+    });
 
-  test('setType() - empty message', () => {
-    const message = '';
-    const type: TypeDefinition = {
-      key: 'fix',
-      title: 'Bug Fixes',
-      description: 'A bug fix',
-    };
+    test('gitmoji, subject and body', () => {
+      const message = ':tada: test message\r\n\r\nbody';
+      const type: TypeDefinition = {
+        key: 'fix',
+        title: 'Bug Fixes',
+        description: 'A bug fix',
+      };
 
-    const result = CommitMessageLib.setType(message, type);
-    expect(result).toBe(`${type.key}: `);
-  });
-
-  test('setType() - existing type', () => {
-    const message = 'chore:';
-    const type: TypeDefinition = {
-      key: 'fix',
-      title: 'Bug Fixes',
-      description: 'A bug fix',
-    };
-
-    const result = CommitMessageLib.setType(message, type);
-    expect(result).toBe(`${type.key}: `);
-  });
-
-  test('setType() - gitmoji', () => {
-    const message = ':tada:';
-    const type: TypeDefinition = {
-      key: 'fix',
-      title: 'Bug Fixes',
-      description: 'A bug fix',
-    };
-
-    const result = CommitMessageLib.setType(message, type);
-    expect(result).toBe(`${type.key}: :tada:`);
-  });
-
-  test('setType() - existing type and gitmoji', () => {
-    const message = 'chore: :tada:';
-    const type: TypeDefinition = {
-      key: 'fix',
-      title: 'Bug Fixes',
-      description: 'A bug fix',
-    };
-
-    const result = CommitMessageLib.setType(message, type);
-    expect(result).toBe(`${type.key}: :tada:`);
-  });
-
-  test('setType() - existing type, gitmoji and subject', () => {
-    const message = 'chore: :tada: test message';
-    const type: TypeDefinition = {
-      key: 'fix',
-      title: 'Bug Fixes',
-      description: 'A bug fix',
-    };
-
-    const result = CommitMessageLib.setType(message, type);
-    expect(result).toBe(`${type.key}: :tada: test message`);
-  });
-
-  test('setType() - existing type, gitmoji, subject and body', () => {
-    const message = 'chore: :tada: test message\r\n\r\nbody';
-    const type: TypeDefinition = {
-      key: 'fix',
-      title: 'Bug Fixes',
-      description: 'A bug fix',
-    };
-
-    const result = CommitMessageLib.setType(message, type);
-    expect(result).toBe(`${type.key}: :tada: test message\r\n\r\nbody`);
-  });
-
-  test('setType() - gitmoji, subject and body', () => {
-    const message = ':tada: test message\r\n\r\nbody';
-    const type: TypeDefinition = {
-      key: 'fix',
-      title: 'Bug Fixes',
-      description: 'A bug fix',
-    };
-
-    const result = CommitMessageLib.setType(message, type);
-    expect(result).toBe(`${type.key}: :tada: test message\r\n\r\nbody`);
+      const result = CommitMessageLib.setType(message, type);
+      expect(result).toBe(`${type.key}: :tada: test message\r\n\r\nbody`);
+    });
   });
 });
--- shared/commit-message.lib.ts
+++ shared/commit-message.lib.ts
@@ -4,14 +4,17 @@ import { GitmojiDefinition } from './presets/gitmojis';
 import { TypeDefinition } from './presets/types';
 
 export class CommitMessageLib {
-  static SUBJECT_REGEX = /^((?![ ]*:).+?(\(.+?\))*?:)?([ ]*)(:\w+:[ ]*)?([\s]*)?([\s\S]*)?/;
+  static SUBJECT_REGEX = /^((?![ ]*:).+?(\(.+?\))*?:)?([ ]*)((:\w+:|\p{Regional_Indicator}\p{Regional_Indicator}|\p{Emoji}(\p{Emoji_Modifier}|\u{FE0F}\u{20E3}?|[\u{E0020}-\u{E007E}]+\u{E007F})?(\u{200D}\p{Emoji}(\p{Emoji_Modifier}|\u{FE0F}\u{20E3}?|[\u{E0020}-\u{E007E}]+\u{E007F})?)*)[ ]*)?([\s]*)?([\s\S]*)?/gu;
 
   static setType(message: string, type: TypeDefinition): string {
-    return (message || '').replace(CommitMessageLib.SUBJECT_REGEX, `${type.key}: $4$5$6`);
+    return (message || '').replace(CommitMessageLib.SUBJECT_REGEX, `${type.key}: $4$9$10`);
   }
 
-  static setGitmoji(message: string, gitmoji: GitmojiDefinition): string {
-    return (message || '').replace(CommitMessageLib.SUBJECT_REGEX, `$1 ${gitmoji.shortcode} $6`);
+  static setGitmoji(message: string, gitmoji: GitmojiDefinition, useShortcode = true): string {
+    return (message || '').replace(
+      CommitMessageLib.SUBJECT_REGEX,
+      `$1 ${useShortcode ? gitmoji.shortcode : gitmoji.icon} $10`,
+    );
   }
 
   static format(commit: Commit, ruleset: QualifiedConfig): string {
