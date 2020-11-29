3dfd8489f571f85ef93df61d9d55fb477ffee508
Jeffrey Ko <jeffreyko@gmail.com>
2020-11-29 15:16:14 +0700

    chore(core): 🔨 add project scopes
    Closes #4

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -3,5 +3,10 @@ module.exports = {
   rules: {
     'header-max-length': [1, 'always', 50],
     'body-max-line-length': [2, 'always', 72],
+    'scope-enum': [
+      2,
+      'always',
+      ['editor', 'config', 'changelog', 'writeup', 'release', 'deps', 'commitlint', 'core'],
+    ],
   },
 };
