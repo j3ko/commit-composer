9999ddbebc2abc4358701a2250bf8407d6424a0e
Jeffrey Ko <jeffreyko@gmail.com>
2020-12-03 22:48:12 +0700

    feat(editor): ✨ load type config into picker

--- shared/presets/types.ts
+++ shared/presets/types.ts
@@ -1,10 +1,15 @@
 import * as list from 'conventional-commit-types';
 
-export type TypeDefinition = {
-  key: string;
+export type TypeEntry = {
   title: string;
   description?: string;
 };
+
+export type TypeDefinition = TypeEntry & {
+  key: string;
+};
+
+export const TYPE_LOOKUP: { [key: string]: TypeEntry } = list.types;
 
 export const TYPES: TypeDefinition[] = Object.keys(list.types).map((key) => {
   return Object.assign({ key }, list.types[key]);
